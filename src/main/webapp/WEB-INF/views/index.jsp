4e5b6c4fcc676ac8cdf384873ca0c7446731ed90
Gunmo Lee <rjsah5676@gmail.com>
2025-02-06 11:58:32 +0900

    mainpage design

--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -27,12 +27,47 @@
 				</div>
 				<div class="main-image"></div>
 				<div class="main-content">
-					<div id="main-content-one">
-						<div id="main-content-one-left"></div>
-						<div id="main-content-one-right"><br><br>&nbsp;&nbsp;편안한 휴식공간 어쩌구</div>
+					<div class="main-content-box" id="main-content-one">
+						<div id="main-content-title">Rest & Comfort</div>
+						<div id="main-content-box-right">
+							<div id="main-content-one-left"></div>
+							<div id="main-content-one-right"><br><br>&nbsp;&nbsp;지친 일상 숙소에서 휴식</div>
+						</div>
 					</div>
-					<div id="main-content-two"></div>
-					<div id="main-content-three"></div>
+					<div class="main-content-box" id="main-content-one-two">
+						<div id="main-content-title">Rest & Comfort</div>
+						<div id="main-content-box-right">
+							<div id="main-content-one-left-two"></div>
+							<div id="main-content-one-right"><br><br>&nbsp;&nbsp;다양한 시설</div>
+						</div>
+					</div>
+					<div class="main-content-box" id="main-content-one-three">
+						<div id="main-content-title">Rest & Comfort</div>
+						<div id="main-content-box-right">
+							<div id="main-content-one-left-three"></div>
+							<div id="main-content-one-right"><br><br>&nbsp;&nbsp;양질의 식사</div>
+						</div>
+					</div>
+					<div class="main-content-box" id="main-content-two">
+						<div id="main-content-title">Visual Amusement</div>
+						<div id="main-content-box-right">
+							<div id="main-content-two-left"></div>
+							<div id="main-content-one-right"><br><br>&nbsp;&nbsp;호텔 내 미술관</div>
+						</div>
+					</div>
+					<div class="main-content-box" id="main-content-two-two">
+						<div id="main-content-title">Visual Amusement</div>
+						<div id="main-content-box-right">
+							<div id="main-content-two-left-two"></div>
+							<div id="main-content-one-right"><br><br>&nbsp;&nbsp;미술 관련 객실</div>
+						</div>
+					</div>
+					<div class="main-content-box" id="main-content-three">
+						<div id="main-content-title">Destination</div>
+						<div id="main-content-road">
+							지도
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -4,8 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
 	main_image=document.getElementsByClassName("main-image")[0];
 	main_content=document.getElementsByClassName("main-content")[0];
 	main_content_one=document.getElementById("main-content-one");
+	main_content_one_two=document.getElementById("main-content-one-two");
+	main_content_one_three=document.getElementById("main-content-one-three");
+	
 	main_content_two=document.getElementById("main-content-two");
+	main_content_two_two=document.getElementById("main-content-two-two");
+	
 	main_content_three=document.getElementById("main-content-three");
+	
 });
 
 var cur_state=0;
@@ -14,7 +20,12 @@ var main_title;
 var main_image;
 var main_content;
 var main_content_one;
+var main_content_one_two;
+var main_content_one_three;
+
 var main_content_two;
+var main_content_two_two;
+
 var main_content_three;
 
 window.addEventListener('resize', function(){
@@ -55,9 +66,9 @@ window.addEventListener("wheel", (e) => {
 				cur_state=1;
 			}
 			if(cur_state==1&&moving++==0) {
-				main_image.style.top = '-2000px';
+				main_image.style.top = '-1600px';
 				main_image.style.transition = 'all 1.5s';
-				main_content.style.height='2700px';
+				main_content.style.height='4000px';
 				main_content_one.style.left='50%';
 				main_content_one.style.opacity=1;
 				setTimeout(function() {
@@ -72,42 +83,96 @@ window.addEventListener("wheel", (e) => {
 let lastScrollY_main=0;
 
 window.addEventListener("scroll", (e) => {
-	let offset=650;
+	let offset=500;
 	let curScrollY = window.scrollY;
 	let header = document.getElementsByClassName("header")[0];
 	let nav = document.getElementsByClassName("nav")[0];
 	if(cur_state == 2) {
-		if(curScrollY<offset) {
+		if(curScrollY<offset) {	//최초 화면
+			main_content_one.style.transition='all 1.5s';
 			main_content_one.style.opacity=1;
+			main_content_one_two.style.opacity=0;
+			main_content_one_three.style.opacity=0;
+			main_content_two.style.opacity=0;
+			main_content_two.style.right='-1000px';
+			main_content_two_two.style.opacity=0;
+			main_content_three.style.opacity=0;
+			/*
 			main_content_two.style.opacity=0;
 			main_content_three.style.opacity=0;
 			main_content_one.style.left='50%';
 			main_content_three.style.left='-1000px';
 			main_content_two.style.right='-1000px';
+			*/
 		}
 		if(curScrollY < offset*2 && curScrollY >= offset) {
+			main_content_one.style.transition='all 1.0s ease-in-out';
 			main_content_one.style.opacity=0;
+			main_content_one_two.style.opacity=1;
+			main_content_one_three.style.opacity=0;
+			main_content_two.style.opacity=0;
+			main_content_two.style.right='-1000px';
+			main_content_two_two.style.opacity=0;
+			main_content_three.style.opacity=0;
+			/*
 			main_content_two.style.opacity=1;
 			main_content_three.style.opacity=0;
 			main_content_one.style.left='-1000px';
 			main_content_three.style.left='-1000px';
 			main_content_two.style.right='50%';
 			main_content_three.style.transition='all 1.5s';
+			*/
 		}
-		if(curScrollY < offset*2 + 400 && curScrollY >= offset*2) {
+		if(curScrollY < offset*3 && curScrollY >= offset*2) {
 			main_content_one.style.opacity=0;
+			main_content_one_two.style.opacity=0;
+			main_content_one_three.style.opacity=1;
+			main_content_two.style.opacity=0;
+			main_content_two.style.right='-1000px';
+			main_content_two_two.style.opacity=0;
+			main_content_three.style.opacity=0;
+			/*
 			main_content_two.style.opacity=0;
 			main_content_three.style.opacity=1;
 			main_content_one.style.left='-1000px';
 			main_content_three.style.left='50%';
 			main_content_two.style.right='-1000px';
 			main_content_three.style.position='fixed';
-			main_content_three.style.top='50%';
+			main_content_three.style.top='50%';*/
 		}
-		if(curScrollY >= offset*2 + 400) {
+		if(curScrollY < offset*4 && curScrollY >= offset*3) {
+			main_content_one.style.opacity=0;
+			main_content_one_two.style.opacity=0;
+			main_content_one_three.style.opacity=0;
+			main_content_two.style.opacity=1;
+			main_content_two.style.right='50%';
+			main_content_two.style.transition='all 1.5s';
+			main_content_two_two.style.opacity=0;
+			main_content_three.style.opacity=0;
+			/*
 			main_content_three.style.position='absolute';
 			main_content_three.style.transition='0.0s';
 			main_content_three.style.top='2150px';
+			*/
+		}
+		if(curScrollY < offset*5 &&curScrollY >= offset*4) {
+			main_content_one.style.opacity=0;
+			main_content_one_two.style.opacity=0;
+			main_content_one_three.style.opacity=0;
+			main_content_two.style.opacity=0;
+			main_content_three.style.opacity=0;
+			main_content_two.style.transition='all 1.0s ease-in-out';
+			main_content_two_two.style.opacity=1;
+			main_content_three.style.left='-1000px';
+		}
+		if(curScrollY>=offset*5) {
+			main_content_one.style.opacity=0;
+			main_content_one_two.style.opacity=0;
+			main_content_one_three.style.opacity=0;
+			main_content_two.style.opacity=0;
+			main_content_two_two.style.opacity=0;
+			main_content_three.style.opacity=1;
+			main_content_three.style.left='50%';
 		}
 	}
 	lastScrollY_main = curScrollY;
