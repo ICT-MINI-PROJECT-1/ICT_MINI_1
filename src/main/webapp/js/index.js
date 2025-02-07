97e9a9fade542e5012a4fbc8122ca913b6b8ecfe
Gunmo Lee <rjsah5676@gmail.com>
2025-02-07 14:10:14 +0900

    add main animation

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -15,9 +15,80 @@ document.addEventListener('DOMContentLoaded', () => {
 	
 	main_content_three=document.getElementById("main-content-three");
 	
+	main_hidden = document.getElementById("main-hidden");
+	
+	addImgListener(main_hidden);
 });
 
+var arr;
+var arr_top = [[53,57,20,12],[23,57,53],[24,59,47],[62,28]];
+var arr_left = [[7,30,23,11],[10,30,3],[8,-5,13],[1,12]];
+
+function moveItem(len,who){
+	arr = new Array();
+	for(var i=0;i<len;i++) {
+		const element = document.getElementById(who+"-box-"+ (i+1));
+		const box = document.getElementById("main-content-one-left-two");
+		const style = getComputedStyle(element);
+		const box_style = getComputedStyle(box);
+		arr.push(style);
+		element.style.top=(getNumberFromPixel(arr[i].top)/(getNumberFromPixel(box_style.height)*20/17))*100-3+(Math.random()*6)%6+"%";
+		element.style.left=(getNumberFromPixel(arr[i].left)/(getNumberFromPixel(box_style.width)*100/60))*100-3+(Math.random()*6)%6+"%";
+	}
+}
+
+function moveItemBack(who,len,idx) {
+	for(var i=0;i<len;i++) {
+		const element = document.getElementById(who+"-box-"+ (i+1));
+		element.style.top=arr_top[idx][i]+"%";
+		element.style.left=arr_left[idx][i]+"%";
+	}
+}
+
+function addImgListener(imgs){
+	imgs.addEventListener('mouseover', function(){
+		if(cur_state == 2){
+			if(where==2)moveItem(4,"two");
+			if(where==3) moveItem(3,"three");
+			if(where==4) moveItem(3,"two-one");
+			if(where==5) moveItem(2,"two-two");
+		}
+	});
+	imgs.addEventListener('mouseout', function(){
+		if(cur_state == 2) {
+			if(where==2) moveItemBack("two",4,0);
+			if(where==3) moveItemBack("three",3,1);
+			if(where==4) moveItemBack("two-one",3,2);
+			if(where==5) moveItemBack("two-two",2,3);
+		}
+	});
+}
+
+function getNumberFromPixel(_px) {
+    if (_px == null || _px == "") {
+        return 0;
+    }
+   
+    _px = _px + "";
+   
+    if (_px.indexOf("px") > -1) {
+        _px = _px.replace("px", "");
+    }
+   
+    if (_px.indexOf("PX") > -1) {
+        _px = _px.replace("PX", "");
+    }
+   
+    var result = parseInt(_px, 10);
+    if ((result + "") == "NaN") {
+        return 0;
+    }
+   
+    return result;
+}
+
 var cur_state=0;
+var where=0;
 
 var main_hello;
 
@@ -32,6 +103,8 @@ var main_content_two;
 var main_content_two_two;
 
 var main_content_three;
+
+var main_hidden;
 
 window.addEventListener('resize', function(){
     if(main_content_three != null) main_content_three.style.transition='all 1.5s';
@@ -74,7 +147,7 @@ window.addEventListener("wheel", (e) => {
 			if(cur_state==1&&moving++==0) {
 				main_image.style.transition = 'all 3.0s';
 				main_image.style.top = '-1600px';
-				main_content.style.height='5400px';
+				main_content.style.height='5000px';
 				main_hello.style.opacity=1;
 				setTimeout(function() {
 				  	moving=0;
@@ -115,34 +188,40 @@ window.addEventListener("scroll", (e) => {
 			main_content_one.style.opacity=1;
 			main_content_one.style.left='50%';
 			main_content_one.style.transition='all 1.5s';
+			where=1;
 		}
 		if(curScrollY < offset*3 && curScrollY >= offset*2) {
 			resetStatus();
 			main_content_one_two.style.opacity=1;
+			where=2;
 		}
 		if(curScrollY < offset*4 && curScrollY >= offset*3) {
 			resetStatus();
 			main_content_one_three.style.opacity=1;
 			main_content_two.style.right='0px';
 			main_content_two.style.transition='all 0.8s ease-in-out';
+			where=3;
 		}
 		if(curScrollY < offset*5 &&curScrollY >= offset*4) {
 			resetStatus();
 			main_content_two.style.opacity=1;
 			main_content_two.style.right='50%';
 			main_content_two.style.transition='all 1.5s';
+			where=4;
 		}
 		if(curScrollY>=offset*5 && curScrollY<offset*6) {
 			resetStatus();
 			main_content_two_two.style.opacity=1;
 			main_content_three.style.left='0px';
 			main_content_three.style.transition='all 0.8s ease-in-out';
+			where=5;
 		}
 		if(curScrollY>=offset*6) {
 			resetStatus();
 			main_content_three.style.opacity=1;
 			main_content_three.style.left='50%';
 			main_content_three.style.transition='all 1.5s';
+			where=6;
 		}
 	}
 	lastScrollY_main = curScrollY;
