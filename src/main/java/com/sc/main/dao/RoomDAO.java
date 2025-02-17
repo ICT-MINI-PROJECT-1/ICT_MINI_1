ac9801be39ebd62c35b8a229c1b58ce3868e5547
Gunmo Lee <rjsah5676@gmail.com>
2025-02-17 14:51:25 +0900

    wish

--- src/main/java/com/sc/main/dao/RoomDAO.java
+++ src/main/java/com/sc/main/dao/RoomDAO.java
@@ -3,7 +3,10 @@ package com.sc.main.dao;
 import java.util.List;
 
 import com.sc.main.vo.RoomVO;
+import com.sc.main.vo.WishlistVO;
 
 public interface RoomDAO {
 	public List<RoomVO> roomSelect(String loc);
+	public RoomVO roomInfo(int roomno);
+	public List<WishlistVO> wishSelect(String userid);
 }
