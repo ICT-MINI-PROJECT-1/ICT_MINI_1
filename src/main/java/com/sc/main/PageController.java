1fbb5d525b0c75f174ff592074d480563a109fee
blackusb <smpaa@naver.com>
2025-02-19 18:14:45 +0900

    push

--- src/main/java/com/sc/main/PageController.java
+++ src/main/java/com/sc/main/PageController.java
@@ -1,13 +1,26 @@
 package com.sc.main;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.sc.main.service.ReviewService;
+import com.sc.main.vo.ReviewVO;
+
 @Controller
 @RequestMapping("/page")
 public class PageController {
+	
+	ModelAndView mav;
+	
+	@Inject
+	ReviewService review_service;
+	
 	@GetMapping("/about")
 	public ModelAndView about(){
 		ModelAndView mav = new ModelAndView();
@@ -49,9 +62,12 @@ public class PageController {
 	
 	@GetMapping("/review")
 	public ModelAndView review(){
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("page","review");
+		List<ReviewVO> list = review_service.reviewSelect();
+		
+		mav = new ModelAndView();
+		mav.addObject("list", list);
 		mav.setViewName("page/review/review_main");
+		
 		return mav;
 	}
 }	
--- src/main/java/com/sc/main/service/ReviewServiceImpl.java
+++ src/main/java/com/sc/main/service/ReviewServiceImpl.java
@@ -1,8 +1,27 @@
 package com.sc.main.service;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Service;
+
+import com.sc.main.dao.ReviewDAO;
+import com.sc.main.vo.ReviewVO;
 
 @Service
 public class ReviewServiceImpl implements ReviewService {
+	@Inject
+	ReviewDAO dao;
+	
+	@Override
+	public List<ReviewVO> reviewSelect() {
+		return dao.reviewSelect();
+	}
+
+	@Override
+	public ReviewVO reviewDetail(int reviewno) {
+		return dao.reviewDetail(reviewno);
+	}
 
 }
