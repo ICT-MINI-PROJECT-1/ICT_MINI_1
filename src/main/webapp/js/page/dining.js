e5103ef5a5fdea06a3afa6beecaef3a5244771c7
KS09-d <qjeltk52@naver.com>
2025-02-20 15:03:39 +0900

    a
    a

--- src/main/webapp/js/page/dining.js
+++ src/main/webapp/js/page/dining.js
@@ -54,42 +54,72 @@ function moveToRest(foodType) {
     f.submit();
 }
 
-/*function 
+//모달값 세팅
+const foods = [
+	"직접 재배한 신선한 야채와 장들을 맘껏 즐길 수 있습니다.",
+	"",
+	"넓고 쾌적한 환경에서 편안한 시간을 보내세요.",
+	"퀴노아로 속을 채운 샬롯, 완두콩, 간장소스를 곁들인 양갈비",
+	"상큼한 소스에 마리네이드하여 숯불에 구운 미니양파, 구운 수제 떡갈비",
+	"흑임자가루를 뿌린 다쿠아즈와 요거트 아이스크림"
+];
 
-	var infoKorean = document.getElementById("korean-page");
-	var infoWestern = document.getElementById("info-western");
-	var infoJapanese = document.getElementById("info-japanese");
+const [thisImg] = {"${pageContext.request.contextPath}/img/page/dining/" + this.img};
 
-	if(foodType=="korean") {
-		infoKorean.style.display = "block";
-		infoWestern.style.display = "none";
-		infoJapanese.style.display = "none";
-	} else if(foodType=="western") {
-		infoKorean.style.display = "none";
-		infoWestern.style.display = "block";
-		infoJapanese.style.display = "none";
-	} else if(foodType=="japanese") {
-		infoKorean.style.display = "none";
-		infoWestern.style.display = "none";
-		infoJapanese.style.display = "block";
+//Ajax
+$(function(){
+	$('a[data-target="#"]').click(function(){
+		$.ajax({
+			
+		});
+	})
+}); 
+	
+	
+
+
+//모달열기
+let orgPosition = '10%';
+
+function openModal() {
+	let modal = document.getElementById("d-modal");
+	let position = window.innerWidth;
+	
+	function move() {
+		if(position <= parseInt(orgPosition, 10)) {
+			modal.style.left = orgPosition;
+			
+		} else {
+			position -= 20;
+			modal.style.left = position + 'px';
+			requestAnimationFrame(move);
+		}
 	}
-}*/
+	modal.style.transition = 'left 0.01s';
+	modal.style.display = 'block';
+	modal.style.left = window.innerwidth + 'px';
+	requestAnimationFrame(move);
+}
 
-/* 사진 확대(모달) */
-/*const modal = document.querySelector(".modal");
-const modalOpen = document.querySelector(".pic-modal");
-const modalClose = document.querySelector(".close-modal");
-
-modalOpen.addEventListener('click', function(){
-	modal.style.display('block');
-});
-
-modalClose.addEventListener('click', function(){
-	modal.style.display('none');
-});*/
-
-
-
+//모달닫기
+function closeModal() {
+	let modal = document.getElementById("d-modal");
+	let position = 0;
+	
+	function move() {
+		if(position >= window.innerWidth) {
+			modal.style.display = 'none';
+			
+		} else {
+		
+			position += 20;
+			modal.style.left = position + 'px';
+			requestAnimationFrame(move);
+		}
+	}
+	modal.style.transition= 'all 0.01s';
+	requestAnimationFrame(move);
+}
 
 
 
