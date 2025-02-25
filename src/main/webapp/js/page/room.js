1a573490156dfa60555771e1ec40b68402435f53
Gunmo Lee <rjsah5676@gmail.com>
2025-02-25 22:16:00 +0900

    모달 움직이기 하는중
    ㅇㅇ

--- src/main/webapp/js/page/room.js
+++ src/main/webapp/js/page/room.js
@@ -7,7 +7,58 @@ document.addEventListener('DOMContentLoaded', () => {
 	grid_2=document.getElementById("room-artnou");
 	grid_3=document.getElementById("room-artdec");
 	grid_4=document.getElementById("room-asian");
+	
+	/*
+	let modal=document.getElementById("room-list-modal");
+	let clicked=0;
+	let f_x=0;
+	let f_y=0;
+	
+	let m_x=0;
+	let m_y=0;
+	
+	let c_x=window.innerWidth/2;
+	let c_y=window.innerHeight/4;
+	
+	let cnt=0;
+	let rect = modal.getBoundingClientRect();
+	
+	modal.addEventListener("mousedown", (e) =>{
+		modal.style.transform="none";
+		if(cnt==0) {
+			modal.style.left = rect.left+rect.width/2+"px";
+			modal.style.top = rect.top+rect.height/2+"px";
+			console.log(modal.style.left);
+			console.log(modal.style.top);
+		}
+		setTimeout(function moveModal(){
+			let cRect = modal.getBoundingClientRect();
+			modal.style.left=c_x+m_x-f_x+'px';
+			modal.style.top=c_y+m_y-f_y+'px';
+			c_x=getNumberFromPixel(modal.style.left);
+			c_y=getNumberFromPixel(modal.style.top);
+			f_x=m_x;
+			f_y=m_y;
+			if(clicked==1)setTimeout(moveModal,10);
+			cnt++;
+		},10);
+		clicked=1;
+		window.addEventListener("mouseup", (e) =>{
+			console.log("!");
+			cnt=0;
+			clicked=0;
+		});
+		let mml = window.addEventListener("mousemove",(e)=>{
+			m_x=e.clientX;
+			m_y=e.clientY;
+			if(cnt==0) {
+				f_x=e.clientX;
+				f_y=e.clientY;
+			}
+		});
+	});*/
 });
+
 var room_wrap;
 var room_title;
 
